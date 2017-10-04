3aba711cc6751dffa8cf3e579ceccdc9090d3482
Jake Lang <phazon@protonmail.com>
2017-10-04 12:54:58 -0400

    Removed dead class constructors

--- src/hera.h
+++ src/hera.h
@@ -51,20 +51,6 @@ public:
 class Hera
 {
 public:
-  /*Hera(struct evm_context *_context)
-  {
-    this->account_exists_fn = _context->fn_table.account_exists;
-    this->get_storage_fn = _context->fn_table.get_storage;  
-    this->set_storage_fn = _context->fn_table.set_storage;
-    this->get_balance_fn = _context->fn_table.get_balance;
-    this->get_code_fn = _context->fn_table.get_code;
-    this->self_destruct_fn = _context->fn_table.selfdestruct;
-    this->call_fn = _context->fn_table.call;  
-    this->get_tx_context_fn = _context->fn_table.get_tx_context;  
-    this->get_block_hash_fn = _context->fn_table.get_block_hash;  
-    this->log_fn = _context->fn_table.log;
-  }*/
-
   Hera(const int abi_vn,
        evm_destroy_fn destroy_fn,
        evm_execute_fn execute_fn,
@@ -89,19 +75,6 @@ public:
   evm_get_code_status_fn get_code_status = nullptr;
   evm_prepare_code_fn prepare_code = nullptr;
   evm_set_option_fn set_option = nullptr;
-
-/*
-  evm_account_exists_fn account_exists_fn = nullptr;
-  evm_get_storage_fn get_storage_fn = nullptr;
-  evm_set_storage_fn set_storage_fn = nullptr;
-  evm_get_balance_fn get_balance_fn = nullptr;
-  evm_get_code_fn get_code_fn = nullptr;
-  evm_selfdestruct_fn self_destruct_fn = nullptr;
-  evm_call_fn call_fn = nullptr;
-  evm_get_tx_context_fn get_tx_context_fn = nullptr;
-  evm_get_block_hash_fn get_block_hash_fn = nullptr;
-  evm_log_fn log_fn = nullptr;
-*/
 };
 
 }
