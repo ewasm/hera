1f5ec12b7bb96bd8888fa005d4d7c528f5a8940c
Jared Wasinger <j-wasinger@hotmail.com>
2018-01-10 13:20:26 +0000

    Build cpp-ethereum and run state tests

---
+++ scripts/tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+git clone --recursive https://github.com/ethereum/cpp-ethereum
+cd cpp-ethereum
+
+rm -rf hera
+ln -s `pwd`/../. hera
+
+mkdir build
+cd build
+cmake -DHERA=ON ..
+make
+
+test/testeth -- --vm hera
