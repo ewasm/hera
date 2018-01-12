f07acdc084f66f871c4891c1b14b72adcdac8580
Alex Beregszaszi <alex@rtfs.hu>
2018-01-12 10:38:43 +0000

    Use hera head commit

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -2,8 +2,11 @@
 
 set -e
 
-git clone --recursive https://github.com/ethereum/cpp-ethereum -b hera
+git clone --recursive https://github.com/ethereum/cpp-ethereum
 cd cpp-ethereum
+
+rm -rf hera
+ln -s `pwd`/../. hera
 
 mkdir build
 cd build
