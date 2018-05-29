3bed39654e128f6635db0b4ffc88e2e302b27220
Jake Lang <phazon@protonmail.com>
2018-05-29 18:23:14 -0400

    make intx not build tests or benchmarks using hacky sed patch on cmake lmoa

--- cmake/ProjectIntX.cmake
+++ cmake/ProjectIntX.cmake
@@ -14,17 +14,19 @@ set(binary_dir ${prefix}/src/intx-build)
 set(intx_include_dir ${source_dir}/include/intx)
 set(intx_lib ${binary_dir}/libs/intx/libintx.a)
 
+set(patch_command sed -i -e "$ d" ${source_dir}/CMakeLists.txt)
 set(build_command cmake --build <BINARY_DIR>)
 set(install_command cmake --build <BINARY_DIR> && cp ${intx_lib} <INSTALL_DIR>)
 
 ExternalProject_Add(IntX
 	PREFIX ${prefix}
 	GIT_REPOSITORY https://github.com/chfast/intx.git
-	GIT_TAG master
+	GIT_TAG 852f43c8f18757938b99b2e5b372ccfb64a47bc2
 	CMAKE_ARGS
 	-DCMAKE_INSTALL_PREFIX=<INSTALL_DIR>
 	SOURCE_DIR ${source_dir}
 	BINARY_DIR ${binary_dir}
+	PATCH_COMMAND ${patch_command}
 	BUILD_COMMAND ${build_command}
 	INSTALL_COMMAND ${install_command}
 	INSTALL_DIR ${prefix}/${CMAKE_INSTALL_LIBDIR}
@@ -38,6 +40,7 @@ set_target_properties(
 	PROPERTIES
 	IMPORTED_LOCATION_RELEASE ${intx_lib}
 	INTERFACE_INCLUDE_DIRECTORIES ${intx_include_dir}
+	INTERFACE_LINK_LIBRARIES ${intx_lib}
 )
 
 add_dependencies(intx::libintx IntX)
