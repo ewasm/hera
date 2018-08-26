ce3b4acd01aa4dcca76e219803ab2b02a80e1d33
Alex Beregszaszi <alex@rtfs.hu>
2018-08-26 23:17:34 +0100

    Use HeraException in EndExecution

--- src/exceptions.h
+++ src/exceptions.h
@@ -22,6 +22,7 @@ namespace hera {
 
 class HeraException : public std::exception {
 public:
+  explicit HeraException(): msg({}) {}
   explicit HeraException(std::string _msg): msg(std::move(_msg)) {}
   const char* what() const noexcept override { return msg.c_str(); }
 protected:
@@ -46,11 +47,8 @@ class ContractValidationFailure : public HeraException {
 class InvalidMemoryAccess : public HeraException {
   using HeraException::HeraException;
 };
-
-class EndExecution : public std::exception {
-public:
-  explicit EndExecution() {}
-  const char* what() const noexcept override { return ""; }
+class EndExecution : public HeraException {
+  using HeraException::HeraException;
 };
 
 /// Static Mode Violation.
