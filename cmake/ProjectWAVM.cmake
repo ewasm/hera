a8170b95ded599384266bebfc81447993446d503
poemm <36397285+poemm@users.noreply.github.com>
2018-09-14 18:20:48 +0000

    update wavm to new version, and related fixes

--- cmake/ProjectWAVM.cmake
+++ cmake/ProjectWAVM.cmake
@@ -20,8 +20,10 @@ set(wasm_library ${binary_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}WASM${CMAKE_STA
 set(ir_library ${binary_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}IR${CMAKE_STATIC_LIBRARY_SUFFIX})
 set(logging_library ${binary_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}Logging${CMAKE_STATIC_LIBRARY_SUFFIX})
 set(unwind_library ${binary_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}WAVMUnwind${CMAKE_STATIC_LIBRARY_SUFFIX})
+set(llvmjit_library ${binary_dir}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}LLVMJIT${CMAKE_STATIC_LIBRARY_SUFFIX})
 
-set(other_libraries ${platform_library} ${wasm_library} ${ir_library} ${logging_library} ${unwind_library})
+set(other_libraries ${platform_library} ${wasm_library} ${ir_library} ${logging_library} ${unwind_library} ${llvmjit_library})
+
 
 ExternalProject_Add(wavm
     PREFIX ${prefix}
@@ -29,8 +31,8 @@ ExternalProject_Add(wavm
     DOWNLOAD_DIR ${prefix}/downloads
     SOURCE_DIR ${source_dir}
     BINARY_DIR ${binary_dir}
-    URL https://github.com/AndrewScheidecker/WAVM/archive/2c77c8a2e49bd291833d79fe6c68801b44ae634c.tar.gz
-    URL_HASH SHA256=044b09afb6b62e0b1ad16dde3ef773f72dd077d7c54c88d6716162caa9eca2e7
+    URL https://github.com/AndrewScheidecker/WAVM/archive/b15e29f2ee361379742957d764aca655ef2a9e48.tar.gz
+    URL_HASH SHA256=fd6aa9261cf6bc21098e0c86ed68383a281dc6720455c75afdcdceeddbbc2dee
     PATCH_COMMAND sh ${CMAKE_CURRENT_LIST_DIR}/patch_wavm.sh
     CMAKE_ARGS
     -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
