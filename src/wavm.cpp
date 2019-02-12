05aa2c4602b4ccc7e5fe621c88dc0f4355ab965c
Alex Beregszaszi <alex@rtfs.hu>
2019-02-12 16:50:28 +0000

    Reorder EEI methods in wavm to match eei.h

--- src/wavm.cpp
+++ src/wavm.cpp
@@ -75,10 +75,8 @@ namespace wavm_host_module {
   // first the ethereum interface(s), the top of the stack is used in host functions
   stack<WavmEthereumInterface*> interface;
 
-
   // the host module is called 'ethereum'
   DEFINE_INTRINSIC_MODULE(ethereum)
-
 
   // host functions follow
   DEFINE_INTRINSIC_FUNCTION(ethereum, "useGas", void, useGas, I64 amount)
@@ -86,90 +84,75 @@ namespace wavm_host_module {
     interface.top()->eeiUseGas(amount);
   }
 
+  DEFINE_INTRINSIC_FUNCTION(ethereum, "getGasLeft", U64, getGasLeft)
+  {
+    return static_cast<U64>(interface.top()->eeiGetGasLeft());
+  }
 
   DEFINE_INTRINSIC_FUNCTION(ethereum, "getAddress", void, getAddress, U32 resultOffset)
   {
     interface.top()->eeiGetAddress(resultOffset);
   }
 
-
-  DEFINE_INTRINSIC_FUNCTION(ethereum, "call", U32, call, I64 gas, U32 addressOffset, U32 valueOffset, U32 dataOffset, U32 dataLength)
+  DEFINE_INTRINSIC_FUNCTION(ethereum, "getCallDataSize", U32, getCallDataSize)
   {
-    return interface.top()->eeiCall(EthereumInterface::EEICallKind::Call, gas, addressOffset, valueOffset, dataOffset, dataLength);
+    return interface.top()->eeiGetCallDataSize();
   }
-
 
   DEFINE_INTRINSIC_FUNCTION(ethereum, "callDataCopy", void, callDataCopy, U32 resultOffset, U32 dataOffset, U32 length)
   {
     interface.top()->eeiCallDataCopy(resultOffset, dataOffset, length);
   }
 
-
-  DEFINE_INTRINSIC_FUNCTION(ethereum, "getCallDataSize", U32, getCallDataSize)
-  {
-    return interface.top()->eeiGetCallDataSize();
-  }
-
-
   DEFINE_INTRINSIC_FUNCTION(ethereum, "getCaller", void, getCaller, U32 resultOffset)
   {
     interface.top()->eeiGetCaller(resultOffset);
   }
-
-
-  DEFINE_INTRINSIC_FUNCTION(ethereum, "getGasLeft", U64, getGasLeft)
-  {
-    return static_cast<U64>(interface.top()->eeiGetGasLeft());
-  }
-
-
-  DEFINE_INTRINSIC_FUNCTION(ethereum, "storageStore", void, storageStore, U32 pathOffset, U32 valueOffset)
-  {
-    interface.top()->eeiStorageStore(pathOffset, valueOffset);
-  }
-
-
-  DEFINE_INTRINSIC_FUNCTION(ethereum, "storageLoad", void, storageLoad, U32 pathOffset, U32 valueOffset)
-  {
-    interface.top()->eeiStorageLoad(pathOffset, valueOffset);
-  }
-
-
-  DEFINE_INTRINSIC_FUNCTION(ethereum, "codeCopy", void, codeCopy, U32 resultOffset, U32 codeOffset, U32 length)
-  {
-    interface.top()->eeiCodeCopy(resultOffset, codeOffset, length);
-  }
-
 
   DEFINE_INTRINSIC_FUNCTION(ethereum, "getCodeSize", U32, getCodeSize)
   {
     return interface.top()->eeiGetCodeSize();
   }
 
+  DEFINE_INTRINSIC_FUNCTION(ethereum, "codeCopy", void, codeCopy, U32 resultOffset, U32 codeOffset, U32 length)
+  {
+    interface.top()->eeiCodeCopy(resultOffset, codeOffset, length);
+  }
+
+  DEFINE_INTRINSIC_FUNCTION(ethereum, "storageStore", void, storageStore, U32 pathOffset, U32 valueOffset)
+  {
+    interface.top()->eeiStorageStore(pathOffset, valueOffset);
+  }
+
+  DEFINE_INTRINSIC_FUNCTION(ethereum, "storageLoad", void, storageLoad, U32 pathOffset, U32 valueOffset)
+  {
+    interface.top()->eeiStorageLoad(pathOffset, valueOffset);
+  }
 
   DEFINE_INTRINSIC_FUNCTION(ethereum, "finish", void, finish, U32 dataOffset, U32 length)
   {
     interface.top()->eeiFinish(dataOffset, length);
   }
 
-
   DEFINE_INTRINSIC_FUNCTION(ethereum, "revert", void, revert, U32 dataOffset, U32 length)
   {
     interface.top()->eeiRevert(dataOffset, length);
   }
-
 
   DEFINE_INTRINSIC_FUNCTION(ethereum, "getReturnDataSize", U32, getReturnDataSize)
   {
     return interface.top()->eeiGetReturnDataSize();
   }
 
-
   DEFINE_INTRINSIC_FUNCTION(ethereum, "returnDataCopy", void, returnDataCopy, U32 resultOffset, U32 dataOffset, U32 length)
   {
     interface.top()->eeiReturnDataCopy(resultOffset, dataOffset, length);
   }
 
+  DEFINE_INTRINSIC_FUNCTION(ethereum, "call", U32, call, I64 gas, U32 addressOffset, U32 valueOffset, U32 dataOffset, U32 dataLength)
+  {
+    return interface.top()->eeiCall(EthereumInterface::EEICallKind::Call, gas, addressOffset, valueOffset, dataOffset, dataLength);
+  }
 
   // this is needed for resolving names of imported host functions
   struct HeraWavmResolver : Runtime::Resolver {
