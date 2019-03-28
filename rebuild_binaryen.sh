58afee24fc59f1473f71a999c3ad5efa8cb16ece
chen xian sun <chenxiansun@chenxiandeMacBook-Pro.local>
2019-03-28 20:30:57 +0800

    rebuild binaryen

---
+++ rebuild_binaryen.sh
@@ -0,0 +1,2 @@
+cd /Users/chenxiansun/go/src/github.com/ewasm/hera/build/deps/src && rm -f -r binaryen-build && cd binaryen-stamp && rm -f binaryen-build  binaryen-configure binaryen-done binaryen-install
+cd /Users/chenxiansun/go/src/github.com/ewasm/hera && ./build_debug.sh
