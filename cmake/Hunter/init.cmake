a318cbddbfad779cc472b1c30371cc6d5516ce34
Paweł Bylica <chfast@gmail.com>
2019-05-14 10:49:11 +0200

    cmake: Add own Hunter version 0.23.169

---
+++ cmake/Hunter/init.cmake
@@ -0,0 +1,12 @@
+
+if(EVMC_TESTING OR EVMC_TEST_TOOLS)
+    set(HUNTER_CONFIGURATION_TYPES Release
+        CACHE STRING "Build type of the Hunter packages")
+
+    include(HunterGate)
+
+    HunterGate(
+        URL "https://github.com/ruslo/hunter/archive/v0.23.169.tar.gz"
+        SHA1 "60de9f275143ab38df559986c04e3726683e7de7"
+    )
+endif()
